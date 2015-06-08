03feac291b84f398725496e4e40d3b006bc2e07d
Walton Seymour <bigw369@gmail.com>
2015-06-08 11:38:04 -0400

    more tests

--- tests.py
+++ tests.py
@@ -2,12 +2,18 @@ import unittest, matchmaker
 
 class MyTest(unittest.TestCase):
     def test_person(self):
-        self.person = matchmaker.Person("person1", [1, 2, 3], is_driver=True, size=2, is_site_leader=True)
-        self.assertEqual(self.person.name, "person1")
-        self.assertEqual(self.person.available_times, [1,2,3])
-        self.assertTrue(self.person.is_driver)
-        self.assertEqual(self.person.size, 2)
-        self.assertTrue(self.person.is_site_leader)
+        person = matchmaker.Person("person1", [1, 2, 3], is_driver=True, size=2, is_site_leader=True)
+        self.assertEqual(person.name, "person1")
+        self.assertEqual(person.available_times, [1,2,3])
+        self.assertTrue(person.is_driver)
+        self.assertEqual(person.size, 2)
+        self.assertTrue(person.is_site_leader)
+
+    def test_site(self):
+        site = matchmaker.Site("site1", meeting_time=1, capacity=4)
+        self.assertEqual(site.name, "site1")
+        self.assertEqual(site.meeting_time, 1)
+        self.assertEqual(site.capacity, 4)
 
 if __name__ == '__main__':
     unittest.main()
